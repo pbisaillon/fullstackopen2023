0cb02e0d9fac4968890529a9e5e15c7519cec48f
Philippe Bisaillon <pbisaillon@protonmail.com>
2024-01-10 16:52:25 -0500

    feat: 2.5 course information step 10

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,30 +1,4 @@
-const Header = ({ course }) => <h2>{course}</h2>
-
-const Content = ({ parts, courseId }) => {
-  return (
-    <ul>
-      {parts.map(part => <li key={"".concat(courseId, '-', part.id)}>{part.name} {part.exercises}</li>)}
-    </ul>
-  )
-}
-const Total = ({ parts }) => {
-  const i = 0
-  const sum = parts.reduce((s, p) => s + p.exercises, i)
-  return (<div>Total of {sum} exercises</div>)
-}
-
-const Course = ({ course }) => {
-  return (
-    <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} courseId={course.id} />
-      <Total parts={course.parts} />
-    </div>
-  )
-}
-
-const Courses = ({ courses }) => courses.map(course => <Course key={course.id} course={course} />)
-
+import Courses from './components/Courses'
 
 const App = () => {
   const courses = [
