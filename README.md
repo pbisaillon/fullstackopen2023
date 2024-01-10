ee11d2f75ca12502eddc30e5510bb3f8cf531f62
Philippe Bisaillon <pbisaillon@protonmail.com>
2024-01-10 17:35:11 -0500

    feat: 2.6 phonebook step 1

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # fullstackopen2023
 My notes and exercises for the Full Stack Open course (https://fullstackopen.com/en)
+
+To inialitize a new project
+
+```shell
+ npm create vite@latest phonebook -- --template react
+ cd phonebook
+ npm install
+ npm run dev
+```
---
+++ part2/phonebook/src/App.jsx
@@ -0,0 +1,44 @@
+import { useState } from 'react'
+
+const Persons = ({ persons }) => persons.map(person => <div key={person.name}> {person.name}</div>)
+
+
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ])
+  const [newName, setNewName] = useState('')
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    const personObject = {
+      name: newName,
+    }
+    setPersons(persons.concat(personObject))
+  }
+
+  const handlePersonChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name: <input value={newName} onChange={handlePersonChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <Persons persons={persons} />
+      <div>debug: {newName}</div>
+    </div>
+  )
+}
+
+export default App
